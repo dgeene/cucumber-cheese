11deb7777fb725c87b1239122c04bed973091dcf
dgeene <david.geene@gmail.com>
2016-05-13 15:43:59 -0400

    complete checkout page

--- test_puppies/features/step_definitions/adopting_puppy_steps.rb
+++ test_puppies/features/step_definitions/adopting_puppy_steps.rb
@@ -21,26 +21,27 @@ end
 
 When(/^I click the Complete the Adoption button$/) do
     @cart.proceed_to_checkout
+    @checkout = CheckoutPage.new(@browser)
 end
 
 When(/^I enter "([^"]*)" in the name field$/) do |name|
-        @browser.text_field(:id => 'order_name').set(name)
+    @checkout.name = name
 end
 
 When(/^I enter "([^"]*)" in the address field$/) do |address|
-        @browser.text_field(:id => 'order_address').set(address)
+    @checkout.address = address
 end
 
 When(/^I enter "([^"]*)" in the email field$/) do |email|
-        @browser.text_field(:id => 'order_email').set(email)
+    @checkout.email = email
 end
 
 When(/^I select "([^"]*)" from the pay with dropdown$/) do |pay_type|
-        @browser.select_list(:id => 'order_pay_type').select(pay_type)
+    @checkout.pay_type = pay_type
 end
 
 When(/^I click the Place Order button$/) do
-        @browser.button(:value => 'Place Order').click
+    @checkout.place_order
 end
 
 Then(/^I should see "([^"]*)"$/) do |expected|
