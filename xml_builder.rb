08f48313eef566a7eafe5adae4b3328727fb087e
dgeene <david.geene@gmail.com>
2016-06-27 19:34:18 -0400

    fix syntax error

--- xml_builder.rb
+++ xml_builder.rb
@@ -2,6 +2,9 @@ require 'builder'
 
 builder = Builder::XmlMarkup.new
 xml = builder.person do |p|
-    p.name = 'Cheezy'
-    p.phone = '555-1234'
+    p.name 'Cheezy'
+    p.phone '555-1234'
 end
+
+
+puts xml
